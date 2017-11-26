39d1231a360e3351479d07b6b791d7871283e464
choijiwung <784wldnd@naver.com>
2017-11-26 11:09:15 +0900

    DAY12-02 화면구성 인강 완료

---
+++ MyBasicWedget/app/src/main/java/com/rememberme/mybasicwedget/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rememberme.mybasicwedget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton2);
+        RadioButton radioButton1 = (RadioButton) findViewById(R.id.radioButton3);
+
+    }
+
+
+}
