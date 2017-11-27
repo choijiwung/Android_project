3a3c0cf9a6abedd34be5f1b69c68975c1cce0679
choijiwung <784wldnd@naver.com>
2017-11-27 21:45:46 +0900

    20171127

--- MyBasicWedget/app/src/main/java/com/rememberme/mybasicwedget/LoginActivity.java
+++ MyBasicWedget/app/src/main/java/com/rememberme/mybasicwedget/LoginActivity.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton2);
         RadioButton radioButton1 = (RadioButton) findViewById(R.id.radioButton3);
