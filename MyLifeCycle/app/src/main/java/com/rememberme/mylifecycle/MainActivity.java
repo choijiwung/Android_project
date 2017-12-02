496a4b68c26907323b9658d1ce0c53e405e9c6d3
choijiwung <784wldnd@naver.com>
2017-12-02 18:32:38 +0900

    42강

---
+++ MyLifeCycle/app/src/main/java/com/rememberme/mylifecycle/MainActivity.java
@@ -0,0 +1,75 @@
+package com.rememberme.mylifecycle;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        수명주기 메소드 onCreate
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate 호출", Toast.LENGTH_LONG).show();
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             finish();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart 호출", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop 호출", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy 호출", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause 호출", Toast.LENGTH_LONG).show();
+//        보통 onPause 상태에서 파일을 저장을 많이한다. shareReferences
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume 호출", Toast.LENGTH_LONG).show();
+
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        if (pref != null){
+            String name = pref.getString("name", "");
+            Toast.makeText(this, "복구된이름"+ name, Toast.LENGTH_LONG).show();
+            
+
+        }
+    }
+//    모든것이 수명주기 메소드
+}
