8ac31fe0534b5ba0a05c2d21294d03698d3139d6
choijiwung <784wldnd@naver.com>
2017-12-03 18:29:59 +0900

    45강완료

---
+++ MySMSReceiver/app/src/main/java/com/rememberme/mysmsreceiver/SmsReceiver.java
@@ -0,0 +1,17 @@
+package com.rememberme.mysmsreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+        private static final  String TAG = "SmsReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive() 호출됨");
+
+
+    }
+}
