d6b272c6883c3eabd486cf4194b91f2d4be53f42
choijiwung <784wldnd@naver.com>
2017-12-03 16:52:14 +0900

    43강완료

--- Myservice/app/src/main/java/com/rememberme/myservice/MainActivity.java
+++ Myservice/app/src/main/java/com/rememberme/myservice/MainActivity.java
@@ -1,13 +1,34 @@
 package com.rememberme.myservice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         editText= (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                intent.putExtra("command", "show");
+//                임의로 넣음
+                intent.putExtra("name", name);
+                startService(intent);
+
+            }
+        });
+
     }
 }
--- Myservice/app/src/main/java/com/rememberme/myservice/MyService.java
+++ Myservice/app/src/main/java/com/rememberme/myservice/MyService.java
@@ -3,9 +3,45 @@ package com.rememberme.myservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+    private static final String TAG = "MYSERVICE";
+
     public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate()호출됨");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand호출됨");
+//      서비스는 계속 실행되니때문에 intent로 정보를 전달할때 확인할수가없다.
+//      그럴때는 onstartCommand를 사용한다.
+        if (intent == null){
+            return Service.START_STICKY;
+//          서비스가 종료 되었을 때 자동으로 다시 실행 된다는 뜻
+        }else{
+            procesCommand(intent);
+        }
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+    public void procesCommand(Intent intent){
+        String command = intent.getStringExtra("command");
+        String name = intent.getStringExtra("name");
+
+        Log.d(TAG,"전달받은 데이터" + command + name);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy()호출됨");
+        super.onDestroy();
     }
 
     @Override
